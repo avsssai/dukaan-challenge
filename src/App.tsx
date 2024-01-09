188eb7d088026f626dee889dc5ff4f44a4911736
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2024-01-09 13:15:03 +0530

    responsive v1

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import {
 	ChevronRight,
 	Download,
 	HelpCircle,
+	Info,
 	MessageSquareMore,
+	MoveDown,
 	SearchIcon,
 } from "lucide-react";
 import Tab from "./components/Tab";
@@ -16,8 +18,8 @@ import Button from "./components/Button";
 function App() {
 	return (
 		<div className='min-h-screen flex'>
-			<section className='basis-[224px] overflow-y-clip bg-navbar min-h-full text-white px-2 py-6 flex flex-col gap-4'>
-				<div className='px-2 flex gap-3 h-[42px] mb-10'>
+			<section className='basis-[224px] overflow-y-clip bg-navbar min-h-full text-white px-2 py-6  flex-col gap-4 hidden md:flex'>
+				<div className='px-2 flex gap-3 h-[42px] mb-2'>
 					<div className='w-[39px] py-[1px] relative'>
 						<img
 							src='/public/Image.png'
@@ -27,7 +29,9 @@ function App() {
 					</div>
 					<div className='flex flex-col justify-between flex-1'>
 						<h6 className='text-[15px] font-medium'>Nishyan</h6>
-						<a href='#' className='text-[13px] underline'>
+						<a
+							href='#'
+							className='text-[12px] lg:text-[13px] underline'>
 							Visit store
 						</a>
 					</div>
@@ -54,31 +58,31 @@ function App() {
 						{Icons["wallet"].icon}
 					</div>
 					<div>
-						<p className='text-subBody text-white'>
+						<p className='text-caption lg:text-subBody text-white'>
 							Available credits
 						</p>
 						<p className='text-subTitle font-medium'>222.10</p>
 					</div>
 				</div>
 			</section>
-			<section className='px-8 w-full overflow-y-scroll'>
+			<section className='px-2 md:px-8 w-full overflow-y-scroll'>
 				<header className='flex justify-evenly items-center gap-4 py-3 h-16 border-b'>
-					<div className='flex items-center flex-1'>
+					<div className='flex flex-col lg:flex-row lg:items-center flex-1'>
 						<h5 className='text-blackFifty text-body mr-4'>
 							Payments
 						</h5>
-						<p className='flex gap-[6px] items-center'>
+						<p className='flex gap-1 md:gap-[6px] items-center'>
 							<HelpCircle
 								color='#4D4D4D'
 								width={12}
 								height={12}
 							/>{" "}
-							<span className='text-caption text-blackFifty'>
+							<span className='text-[9px] sm:text-[11px] lg:text-caption text-blackFifty'>
 								How it works
 							</span>
 						</p>
 					</div>
-					<div className='relative flex-1'>
+					<div className='relative flex-auto lg:flex-1'>
 						<input
 							className='h-10 w-full rounded-md px-10 py-[9px] gap-2 relative placeholder:text-grayFifty text-body leading-[22px]'
 							placeholder='Search features, tutorials, etc.'
@@ -120,7 +124,7 @@ function App() {
 						/>
 					</div>
 				</div>
-				<div className='flex gap-5 mb-8'>
+				<div className='flex flex-col sm:flex-row gap-5 mb-8'>
 					<InfoCard title='Online orders' info='231' />
 					<InfoCard title='Amount received' info='₹23,92,312.19' />
 				</div>
@@ -129,8 +133,8 @@ function App() {
 						Transactions | This Month
 					</h2>
 					<div className='p-3 pb-6 mb-3'>
-						<div className='flex items-center'>
-							<div className='relative max-w-[248px] mr-auto mb-3'>
+						<div className='flex md:items-center mb-3 flex-col sm:flex-row gap-2'>
+							<div className='relative max-w-[248px] mr-auto '>
 								<input
 									className='h-10 border-2 rounded-sm px-10 py-[9px] gap-2 relative placeholder:text-grayFifty text-body leading-[22px]'
 									placeholder='Search by order ID...'
@@ -142,7 +146,7 @@ function App() {
 									className='absolute top-[13px] left-4'
 								/>
 							</div>
-							<div className='flex gap-3'>
+							<div className='flex  gap-3'>
 								<Button type='text'>
 									<span className='text-blackThirty mr-1.5'>
 										Sort
@@ -166,14 +170,22 @@ function App() {
 						</div>
 						<table className='table-fixed w-full'>
 							<thead>
-								<tr className='h-10  bg-whiteCream text-blackThirty text-normal font-medium'>
+								<tr className='h-10  bg-whiteCream text-blackThirty text-[10px] sm:text-normal font-medium'>
 									<th className='text-start pl-3'>
 										Order ID
 									</th>
-									<th className='text-start'>Order date</th>
+									<th className='text-start flex items-center my-auto h-10'>
+										Order date{" "}
+										<MoveDown width={12} height={12} />
+									</th>
 									<th className='text-end'>Order amount</th>
-									<th className='text-end pr-3'>
+									<th className='text-end pr-[18px] relative'>
 										Transaction fees
+										<Info
+											width={14}
+											height={14}
+											className='absolute right-0 top-3.5 ml-1'
+										/>
 									</th>
 								</tr>
 							</thead>
@@ -202,7 +214,7 @@ function App() {
 						</table>
 					</div>
 				</div>
-				<div className='pagination flex gap-6 items-center justify-center mb-8'>
+				<div className='pagination flex gap-6 items-center justify-center mb-8 flex-wrap'>
 					<Button type='nav'>
 						<span>
 							<ChevronLeft
@@ -217,14 +229,16 @@ function App() {
 						<Button type='number'>1</Button>
 						<Button type='number'>...</Button>
 						<Button type='number-selected'>10</Button>
-						<Button type='number'>11</Button>
-						<Button type='number'>12</Button>
-						<Button type='number'>13</Button>
-						<Button type='number'>14</Button>
-						<Button type='number'>15</Button>
-						<Button type='number'>16</Button>
-						<Button type='number'>17</Button>
-						<Button type='number'>18</Button>
+						<div className='hidden md:flex'>
+							<Button type='number'>11</Button>
+							<Button type='number'>12</Button>
+							<Button type='number'>13</Button>
+							<Button type='number'>14</Button>
+							<Button type='number'>15</Button>
+							<Button type='number'>16</Button>
+							<Button type='number'>17</Button>
+							<Button type='number'>18</Button>
+						</div>
 					</div>
 					<Button type='nav'>
 						<span className='text-blackThirty'>Next</span>
